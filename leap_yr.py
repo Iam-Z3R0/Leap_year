f5e472f29abf1042a06b224eede3a59fbff143a9
JohnAsare <johnasare7@gmail.com>
2018-07-14 19:43:51 -0400

    "Wrote down the author name and a describtion about the code "

---
+++ leap_yr.py
@@ -0,0 +1,8 @@
+# Author: John Asare
+# Date: Sat Jul 14 19:40
+
+
+""" Coding algorithm to check if a year is a leap year. This code is written to best of my ability. I am coded these by
+referencing it from a book but, with a strong understanding and reasoning.
+"""
+
