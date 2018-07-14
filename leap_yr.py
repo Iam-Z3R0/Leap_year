6ace416e23b6a6ddd8e1f785114fa16d89396949
JohnAsare <johnasare7@gmail.com>
2018-07-14 19:55:23 -0400

    "Added a source link to the documentation"

--- leap_yr.py
+++ leap_yr.py
@@ -4,5 +4,6 @@
 
 """ Coding algorithm to check if a year is a leap year. This code is written to best of my ability. I am coded these by
 referencing it from a book but, with a strong understanding and reasoning.
+check the source: https://www.timeanddate.com/date/leapyear.html
 """
 
