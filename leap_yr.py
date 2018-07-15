1e1107a8046c8cfd247a4bef8830c51a147addc0
JohnAsare <johnasare7@gmail.com>
2018-07-14 21:25:30 -0400

    "Tried to make sure it takes all the three account of Georgia Calendar requirement of leap year. "

--- leap_yr.py
+++ leap_yr.py
@@ -11,18 +11,19 @@ check the source: https://www.timeanddate.com/date/leapyear.html
 def is_leap(year):
     leap = False
 
-    # Check if the year enter is between 1900 and 100000. if not then print a message
     if 1900 >= year >= 10**5:
-        print("Please, type a proper year. Constraints: 1900 <= year <= 10^5")
+        print("The year has to be between 1900 and 100000")
 
-    # check if the year is divisible by 4 or 100 or 400
-    elif year % 4 == 0 or year % 100 == 0 or year % 400 == 0:
+    elif year % 4 == 0:
         leap = True
-    elif year % 4 != 0 or year % 100 != 0 or year % 400 != 0:
+    elif year % 100 == 0 and year % 400 == 0:
+        leap = True
+    else:
         leap = False
 
     return leap
 
 
-year = int(input("Please enter a year: "))
+year = int(input("Please, enter a year: "))
 print(is_leap(year))
+
