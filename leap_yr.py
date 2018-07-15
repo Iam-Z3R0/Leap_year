83bf3b69b99a7de03fa25481bd2e20a6c2d8653a
JohnAsare <johnasare7@gmail.com>
2018-07-14 20:52:59 -0400

    "The code now can check if its a divisible by 4 or 100 or 400 and return True as for leap year"

--- leap_yr.py
+++ leap_yr.py
@@ -7,3 +7,23 @@ referencing it from a book but, with a strong understanding and reasoning.
 check the source: https://www.timeanddate.com/date/leapyear.html
 """
 
+
+def is_leap(year):
+    leap = False
+
+    # Check if the year enter is between 1900 and 100000. if not then print a message
+    if 1900 >= year >= 10**5:
+        print("Please, type a proper year. Constraints: 1900 <= year <= 10^5")
+
+    # check if the year is divisible by 4 or 100 or 400
+    elif year % 4 == 0 or year % 100 == 0 or year % 400 == 0:
+        leap = True
+    elif year % 4 == 0
+    else:
+        leap = False
+
+    return leap
+
+
+year = int(input("Please enter a year: "))
+print(is_leap(year))
